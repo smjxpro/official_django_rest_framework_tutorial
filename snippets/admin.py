7f62075f738a8b7d4a0e6034fd4329cac1d0c03f
S. M. JAHANGIR <smjxpro@gmail.com>
2020-02-09 17:39:00 +0600

    added object level permission

--- snippets/admin.py
+++ snippets/admin.py
@@ -1,1 +1,5 @@
-# Register your models here.
+from django.contrib import admin
+
+from snippets.models import Snippet
+
+admin.site.register(Snippet)
--- snippets/views.py
+++ snippets/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from rest_framework import permissions
 
 from snippets.models import Snippet
+from snippets.permissions import IsOwnerOrReadOnly
 from snippets.serializers import SnippetSerializer, UserSerializer
 
 
@@ -18,7 +19,7 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
 
 class UserList(generics.ListAPIView):
