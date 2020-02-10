bc1a720e73aa4730a6f915bb7abf29f90000c28d
S. M. JAHANGIR <smjxpro@gmail.com>
2020-02-10 11:27:20 +0600

    named URL patterns

--- snippets/urls.py
+++ snippets/urls.py
@@ -5,11 +5,11 @@ from snippets import views
 
 urlpatterns = [
     path('', views.api_root),
-    path('snippets/', views.SnippetList.as_view()),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
-    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view()),
-    path('users/', views.UserList.as_view()),
-    path('users/<int:pk>/', views.UserDetail.as_view()),
+    path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
+    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view(), name='snippet-highlight'),
+    path('users/', views.UserList.as_view(), name='user-list'),
+    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
